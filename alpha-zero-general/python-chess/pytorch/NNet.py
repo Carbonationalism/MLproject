12cd0909e860aa09002aad58b22460913d233a0b
mcCarbonationalism <cai0004@auburn.edu>
2020-04-09 20:00:36 -0500

    fixed input training boards as chess boards not ndarrays

--- alpha-zero-general/python-chess/pytorch/NNet.py
+++ alpha-zero-general/python-chess/pytorch/NNet.py
@@ -55,7 +55,9 @@ class NNetWrapper(NeuralNet):
 			while batch_idx < int(len(examples)/args.batch_size):
 				sample_ids = np.random.randint(len(examples), size=args.batch_size)
 				boards, pis, vs = list(zip(*[examples[i] for i in sample_ids]))
-				boards = torch.FloatTensor(np.array(boards).astype(np.float64))
+				## TODO: probably move array conversion out of here to where examples are appended
+				# but then we can't keep game history stored in the boards so idk
+				boards = torch.FloatTensor(np.array([b.toarray() for b in boards]).astype(np.float64))
 				target_pis = torch.FloatTensor(np.array(pis))
 				target_vs = torch.FloatTensor(np.array(vs).astype(np.float64))
 
